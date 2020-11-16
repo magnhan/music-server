13f33a5832ae7cbfa85fd87f222daed68e9e951b
magnhan <45832668+magnhan@users.noreply.github.com>
2020-11-17 00:47:02 +0100

    Removed gitignored files and updated gitignore

--- backend/dist/models/Post.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/*const mongoose = require("mongoose")
-
-const schema = mongoose.Schema({
-    year: String
-})
-
-module.exports = mongoose.model("Music", schema)*/ 
-//# sourceMappingURL=Post.js.map
