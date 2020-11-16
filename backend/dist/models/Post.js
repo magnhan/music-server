b82bc63493bce5f3da9ddeb53e276db66147d828
magnhan <45832668+magnhan@users.noreply.github.com>
2020-11-17 00:38:24 +0100

    Added dist

---
+++ backend/dist/models/Post.js
@@ -0,0 +1,9 @@
+"use strict";
+/*const mongoose = require("mongoose")
+
+const schema = mongoose.Schema({
+    year: String
+})
+
+module.exports = mongoose.model("Music", schema)*/ 
+//# sourceMappingURL=Post.js.map
